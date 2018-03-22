b5d23061df0bdb8528ac67f95cefaacdf6eb3cfa
DonatasT <donatas.tamosauskas@gmail.com>
2018-03-22 23:03:49 +0200

    Made first  contact with frontend and received input form.

---
+++ src/main/java/com/swedbank/itacademy/leasing/demoApp/beans/documents/IncomingLeasingForm.java
@@ -0,0 +1,122 @@
+package com.swedbank.itacademy.leasing.demoApp.beans.documents;
+
+public class IncomingLeasingForm {
+
+        private String customerType;
+        private String assetType;
+        private String carBrand;
+        private String carModel;
+        private String manufacturedDate;
+        private String enginePower;
+        private String assetPrice;
+        private String advancePaymentPercentage;
+        private String advancePaymentAmount;
+        private String leasePeriodInMonths;
+        private String margin;
+        private String contractFee;
+        private String paymentDate;
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getAssetType() {
+        return assetType;
+    }
+
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getManufacturedDate() {
+        return manufacturedDate;
+    }
+
+    public void setManufacturedDate(String manufacturedDate) {
+        this.manufacturedDate = manufacturedDate;
+    }
+
+    public String getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(String enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public String getAssetPrice() {
+        return assetPrice;
+    }
+
+    public void setAssetPrice(String assetPrice) {
+        this.assetPrice = assetPrice;
+    }
+
+    public String getAdvancePaymentPercentage() {
+        return advancePaymentPercentage;
+    }
+
+    public void setAdvancePaymentPercentage(String advancePaymentPercentage) {
+        this.advancePaymentPercentage = advancePaymentPercentage;
+    }
+
+    public String getAdvancePaymentAmount() {
+        return advancePaymentAmount;
+    }
+
+    public void setAdvancePaymentAmount(String advancePaymentAmount) {
+        this.advancePaymentAmount = advancePaymentAmount;
+    }
+
+    public String getLeasePeriodInMonths() {
+        return leasePeriodInMonths;
+    }
+
+    public void setLeasePeriodInMonths(String leasePeriodInMonths) {
+        this.leasePeriodInMonths = leasePeriodInMonths;
+    }
+
+    public String getMargin() {
+        return margin;
+    }
+
+    public void setMargin(String margin) {
+        this.margin = margin;
+    }
+
+    public String getContractFee() {
+        return contractFee;
+    }
+
+    public void setContractFee(String contractFee) {
+        this.contractFee = contractFee;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
